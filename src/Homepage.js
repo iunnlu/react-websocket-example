c2c4478523402623240773596e206ebf3a983442
İlhan Ünlü <iunlu@192.168.1.31>
2024-03-11 22:51:59 +0300

    edited homepage

---
+++ src/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Kardelen Ayşeyi çok seviyorum</h1>
+        <h1>Ama o çok manyak biri</h1>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
